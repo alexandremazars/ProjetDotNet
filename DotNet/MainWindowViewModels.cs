eef6e2567b3f2ec1deb986618f8626ed13aa4d39
lecalvmy <42962272+lecalvmy@users.noreply.github.com>
2018-09-05 16:08:24 +0200

    button start

--- DotNet/MainWindowViewModels.cs
+++ DotNet/MainWindowViewModels.cs
@@ -14,15 +14,20 @@ namespace DotNet
     {
         #region public fields
         public ObservableCollection<IOption> AvailableOptions { get; private set; }
-        public double strike { get; set; }
+        public string strike { get; set; }
         public DateTime maturity { get; set; }
         public decimal price { get; set; }
         //public ObservableCollection<>
         #endregion
 
+        #region Private fields
+        private bool tickerStarted;
+        #endregion 
+
         #region Public Constructors
         public MainWindowViewModels()
         {
+            StartCommand = new DelegateCommand(StartTicker, CanStartTicker);
             IOption call = new VanillaCall("Vanilla", new Share("VanillaShare", "1"), new DateTime(2019, 1, 6), 10);
             List<IOption> myOptionsList = new List<IOption>() { call };
             AvailableOptions = new ObservableCollection<IOption>(myOptionsList);
@@ -30,6 +35,31 @@ namespace DotNet
         }
         #endregion
 
+        public DelegateCommand StartCommand { get; private set; }
+
+        public bool TickerStarted
+        {
+            get { return tickerStarted; }
+            set
+            {
+                SetProperty(ref tickerStarted, value);
+                StartCommand.RaiseCanExecuteChanged();
+
+            }
+
+        }
+        private bool CanStartTicker()
+        {
+            /*while(strike == null || maturity == null )
+            {
+                TickerStarted = false;
+            }
+            return TickerStarted;
+        }*/
+        private void StartTicker()
+        {
+            TickerStarted = true;
+        }
         Main test = new Main();
     }
 }
